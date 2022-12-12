269fcd6856b156378ed12a6ee21468154ee733d6
ZiadSheriif <zsherif308@gmail.com>
2022-12-12 23:26:43 +0200

    concantenate arabic text

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Image-Processing
+
+### install tasseract
+
+1. [Download Tesseract](https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w64-setup-v5.2.0.20220712.exe)
+2. pip install pytesseract
+3. Put in path 
+   - [video](https://1drv.ms/v/s!Al5IcWk-NkJ9mCv-S59t2ZFan8AU)
+
+#### If using conda 
+##### Install with conda (via [conda-forge](https://anaconda.org/conda-forge/pytesseract)):
+- conda install -c conda-forge pytesseract
--- ocr.py
+++ ocr.py
@@ -3,19 +3,23 @@ from PIL import Image
 import os
 from bidi.algorithm import get_display
 import arabic_reshaper
-import sys
 
 
-def ocr(dir, lang):
+def ocr(dir, langSelected):
 
-    # temp directory till put in env. variables
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR.\tesseract.exe'
+    # if it doesn't work :
+    # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR.\tesseract.exe'
+
     for filename in os.scandir(dir):
         try:
             res = pytesseract.image_to_string(
-                Image.open(filename.path), lang=lang)
-            if(lang == 'ara'):
-                res = res[::-1]
+                Image.open(filename.path), lang=langSelected)
+
+            # print(arabic)
+            if(langSelected == 'ara'):
+                arabicText = get_display(res)
+                res = arabic_reshaper.reshape(arabicText)
+
             print(res)
         except Exception:
             print("Error!")
