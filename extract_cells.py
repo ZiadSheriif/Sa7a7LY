8bb9a73d4a8950257f3a3dafcab58e12be74d634
ZiadSheriif <zsherif308@gmail.com>
2022-12-12 17:05:43 +0200

    add dataset of bubble sheet

--- extract_cells.py
+++ extract_cells.py
@@ -10,6 +10,7 @@ from os.path import isfile, join
 from utils.commonfunctions import *
 mpl.rcParams['image.cmap'] = 'gray'
 
+
 def getLines(img, binaryImg, x):
     # Kernel Length
     kernelLength = np.array(img).shape[1] // x
@@ -18,7 +19,8 @@ def getLines(img, binaryImg, x):
     verticalKernel = cv.getStructuringElement(cv.MORPH_RECT, (1, kernelLength))
 
     # Horizontal Kernel (kernelLength x 1)
-    horizontalKernel = cv.getStructuringElement(cv.MORPH_RECT, (kernelLength, 1))
+    horizontalKernel = cv.getStructuringElement(
+        cv.MORPH_RECT, (kernelLength, 1))
 
     # Apply erosion then dilation to detect vertical lines using the vertical kernel
     erodedImg = cv.erode(binaryImg, verticalKernel, iterations=3)
@@ -27,8 +29,9 @@ def getLines(img, binaryImg, x):
     # Apply erosion then dilation to detect horizontal lines using the horizontal kernel
     erodedImg = cv.erode(binaryImg, horizontalKernel, iterations=3)
     horizontalLinesImg = cv.dilate(erodedImg, horizontalKernel, iterations=3)
-    
+
     return verticalLinesImg, horizontalLinesImg
+
 
 def getIntersections(pixels):
     intersections = {}
@@ -40,18 +43,18 @@ def getIntersections(pixels):
                 else:
                     intersections[i] = []
                     intersections[i].append((i, j))
-    
+
     keys = list(intersections.keys())
     uniqueKeys = []
     uniqueKeys.append(keys[0])
     for i in range(1, len(keys), 1):
         if (keys[i] - keys[i - 1] > 5):
             uniqueKeys.append(keys[i])
-    
+
     rows = []
     for i in uniqueKeys:
         rows.append(intersections[i])
-                
+
     finalIntersections = []
     index = 0
     for row in rows:
@@ -61,23 +64,23 @@ def getIntersections(pixels):
             if (row[i][1] - row[i - 1][1] > 5):
                 finalIntersections[index].append(row[i])
         index += 1
-    
+
     return finalIntersections
 
 
 def houghLines(img, type):
-    lines = cv.HoughLinesP(img.astype(np.uint8), 0.5, np.pi/180, 100, 
-    minLineLength=0.25*min(img.shape[0], img.shape[1]), maxLineGap=10)
-    
+    lines = cv.HoughLinesP(img.astype(np.uint8), 0.5, np.pi/180, 100,
+                           minLineLength=0.25*min(img.shape[0], img.shape[1]), maxLineGap=10)
+
     hough_lines_out = np.zeros(img.shape)
     for line in lines:
         x1, y1, x2, y2 = line[0]
         if (type == "vertical"):
             cv.line(hough_lines_out, (x1, 0),
-                (x2, img.shape[0]), (255, 255, 255), 1)
+                    (x2, img.shape[0]), (255, 255, 255), 1)
         else:
             cv.line(hough_lines_out, (0, y1),
-                (img.shape[1], y2), (255, 255, 255), 1)
+                    (img.shape[1], y2), (255, 255, 255), 1)
     return hough_lines_out
 
 
@@ -85,16 +88,18 @@ def runGetIntersections(imgPath):
     img = cv.imread(imgPath, 0)
 
     # thresholding
-    (thresh, binaryImg) = cv.threshold(img, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+    (thresh, binaryImg) = cv.threshold(
+        img, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
     binaryImg = 255 - binaryImg
 
-    verticalLinesImg, horizontalLinesImg = getLines(img, binaryImg, x = 10)
-    
+    verticalLinesImg, horizontalLinesImg = getLines(img, binaryImg, x=10)
+
     verticalLinesImg = houghLines(verticalLinesImg, "vertical")
     horizontalLinesImg = houghLines(horizontalLinesImg, "horizontal")
 
     return binaryImg, verticalLinesImg, horizontalLinesImg, cv.bitwise_and(verticalLinesImg, horizontalLinesImg)
-    
+
+
 def runGetCells(img, intersections):
     cells = {}
     intersections = np.array(intersections)
@@ -113,12 +118,14 @@ def runGetCells(img, intersections):
             cell[0:height, 0:12] = 0
             cell[0:height, -5:width] = 0
             cells[col].append(cell)
-            
-    labels = ["Cells/Code/", "Cells/StudentName/", "Cells/EnglishName/", "Cells/1/", "Cells/2/", "Cells/3/"]
+
+    labels = ["Cells/Code/", "Cells/StudentName/",
+              "Cells/EnglishName/", "Cells/1/", "Cells/2/", "Cells/3/"]
     for key in list(cells.keys()):
         for i in range(len(cells[key])):
             cv.imwrite(labels[key] + str(i) + ".jpg", cells[key][i])
-        
+
+
 def deleteFiles():
     files = glob.glob('Intersections/')
     for file in files:
@@ -126,9 +133,10 @@ def deleteFiles():
     files = glob.glob('verticalLines/')
     for file in files:
         os.remove(file)
-    files=glob.glob('horizontalLines/')
+    files = glob.glob('horizontalLines/')
     for file in files:
         os.remove(file)
+
 
 def run():
     mypath = "SingleInput"
@@ -138,12 +146,14 @@ def run():
     binaryImgs = "binaryImgs/"
     files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     for fileName in files:
-        img, vertical, horizontal, result_image = runGetIntersections(mypath + "/" + fileName)
+        img, vertical, horizontal, result_image = runGetIntersections(
+            mypath + "/" + fileName)
         positions = getIntersections(result_image)
         runGetCells(img, positions)
         cv.imwrite(verticalLines + fileName, vertical)
         cv.imwrite(horizontalLines + fileName, horizontal)
         cv.imwrite(intersections + fileName, result_image)
         cv.imwrite(binaryImgs + fileName, img)
-        
+
+
 run()
